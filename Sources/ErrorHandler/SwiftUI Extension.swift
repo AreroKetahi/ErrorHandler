e7a15f378ff3bd27d3f47eaebed48a358df92268
Arkivili Collindort <isoblack1@outlook.com>
2026-05-13 13:08:26 +0100

    Update SwiftUI Extension.swift

--- Sources/ErrorHandler/SwiftUI Extension.swift
+++ Sources/ErrorHandler/SwiftUI Extension.swift
@@ -17,14 +17,31 @@ extension ErrorHandler {
     struct ErrorHandlerModifier: ViewModifier {
         @Bindable var handler: ErrorHandler
         func body(content: Content) -> some View {
-            content.alert(isPresented: $handler.isPresent, error: handler.error) {
-                Button("OK") {
-                    print(handler.error as Any)
-                    if let action = handler.confirmAction {
-                        action()
+            if #available(iOS 26.0, macOS 26.0, watchOS 26.0, tvOS 26.0, visionOS 26.0, *) {
+                content.alert(isPresented: $handler.isPresent, error: handler.error) {
+                    Button(role: .confirm) {
+#if DEBUG
+                        print(handler.error as Any)
+#endif
+                        if let action = handler.confirmAction {
+                            action()
+                        }
+                        handler.error = nil
+                        handler.isPresent = false
                     }
-                    handler.error = nil
-                    handler.isPresent = false
+                }
+            } else {
+                content.alert(isPresented: $handler.isPresent, error: handler.error) {
+                    Button("OK") {
+#if DEBUG
+                        print(handler.error as Any)
+#endif
+                        if let action = handler.confirmAction {
+                            action()
+                        }
+                        handler.error = nil
+                        handler.isPresent = false
+                    }
                 }
             }
         }
